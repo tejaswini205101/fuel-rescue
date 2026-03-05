a33e7b0e6e2602d270736303ce3d5cd61121508c
tejaswini205101 <161295068+tejaswini205101@users.noreply.github.com>
2026-03-05 18:45:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -65,3 +65,57 @@ document.getElementById("fuelForm").addEventListener("submit", function(e) {
   document.getElementById("message").innerText =
   "Fuel request sent successfully!";
 });
+function simulateDelivery() {
+
+let lat = 12.97;
+let lon = 77.59;
+
+let marker = L.marker([lat, lon]).addTo(map)
+.bindPopup("Fuel Delivery Vehicle");
+
+setInterval(() => {
+lat += 0.001;
+lon += 0.001;
+
+marker.setLatLng([lat, lon]);
+
+}, 2000);
+
+}
+function logout(){
+localStorage.removeItem("role");
+window.location.href="login.html";
+}
+function toggleChat(){
+let box = document.getElementById("chatbox");
+
+if(box.style.display === "none"){
+box.style.display = "block";
+}else{
+box.style.display = "none";
+}
+}
+
+function sendMessage(){
+
+let input = document.getElementById("userInput");
+let message = input.value;
+
+let chat = document.getElementById("messages");
+
+chat.innerHTML += "<p><b>You:</b> " + message + "</p>";
+
+let response = "I can help with fuel delivery or mechanic service.";
+
+if(message.toLowerCase().includes("fuel")){
+response = "Click the Fuel Request form to order fuel ⛽";
+}
+
+if(message.toLowerCase().includes("mechanic")){
+response = "You can request a mechanic from the services section 🔧";
+}
+
+chat.innerHTML += "<p><b>Bot:</b> " + response + "</p>";
+
+input.value = "";
+}
